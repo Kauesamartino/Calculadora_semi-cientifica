43bf671577c3a553b40107520e806bbfc3a567e2
Kauesamartino <kaue_samartino@hotmail.com>
2025-09-19 23:27:54 -0300

    feat: declare atributes

--- CalculadoraForm.cs
+++ CalculadoraForm.cs
@@ -1,8 +1,10 @@
 namespace Calculadora
 {
     public partial class CalculadoraForm : Form
-    {
-        
+    {   
+        private double valor1 = 0;
+        private double valor2 = 0;
+        private string operacao = "";
 
         public CalculadoraForm()
         {
@@ -11,7 +13,7 @@ namespace Calculadora
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            
         }
 
         private void button17_Click(object sender, EventArgs e)
